b53dac09204a6c4997c2a2431430630d92bac935
Mike0001-droid <majorov-2004@list.ru>
2025-10-28 15:59:16 +0500

    add company_activity and CompanyPhone to init file in database

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,4 +1,4 @@
 from .database import Base, engine, AsyncSessionLocal, get_db, create_tables
 from .models.activity import Activity
 from .models.building import Building
-from .models.company import Company
+from .models.company import Company, company_activity, CompanyPhone
